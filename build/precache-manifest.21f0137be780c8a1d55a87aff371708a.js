f0ac8753deaea039c2b7a81f93c38c189c898709
amitnishu15 <amitnishu15@gmail.com>
2020-12-31 19:31:41 +0530

    second commit

---
+++ build/precache-manifest.21f0137be780c8a1d55a87aff371708a.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c03bdd6ff894541c0ea0ce105c07d61",
+    "url": "/sonika/index.html"
+  },
+  {
+    "revision": "138969dd68fc2e266b21",
+    "url": "/sonika/static/css/2.63147665.chunk.css"
+  },
+  {
+    "revision": "aa7e0b1cf64688554a75",
+    "url": "/sonika/static/css/main.daa67793.chunk.css"
+  },
+  {
+    "revision": "138969dd68fc2e266b21",
+    "url": "/sonika/static/js/2.9cb04a33.chunk.js"
+  },
+  {
+    "revision": "1a0968dc243fc785d05284e154e37292",
+    "url": "/sonika/static/js/2.9cb04a33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7e0b1cf64688554a75",
+    "url": "/sonika/static/js/main.002b6972.chunk.js"
+  },
+  {
+    "revision": "e97d4775e9039f45bddc",
+    "url": "/sonika/static/js/runtime-main.d164975f.js"
+  },
+  {
+    "revision": "ae94ddabd6f007b2fc4b3e332fd834b6",
+    "url": "/sonika/static/media/e-profile.ae94ddab.png"
+  },
+  {
+    "revision": "11825ab66da0c26763023d4888ef8cc8",
+    "url": "/sonika/static/media/e1.11825ab6.jpg"
+  },
+  {
+    "revision": "f47d5d63a68cc5fa92d7d3f158612004",
+    "url": "/sonika/static/media/e10.f47d5d63.jpg"
+  },
+  {
+    "revision": "052b7bcb0c77dfa4271c32b8b263f8a4",
+    "url": "/sonika/static/media/e11.052b7bcb.jpg"
+  },
+  {
+    "revision": "5c5732e450866595a56fdf41a9b7f7b0",
+    "url": "/sonika/static/media/e12.5c5732e4.jpg"
+  },
+  {
+    "revision": "21f9ea7cfedfcdbb5153933058cb4c9c",
+    "url": "/sonika/static/media/e13.21f9ea7c.jpg"
+  },
+  {
+    "revision": "10cbbc6bc7cfa7bd94bf81a03e1b2d48",
+    "url": "/sonika/static/media/e14.10cbbc6b.jpg"
+  },
+  {
+    "revision": "ea629b0512bc1f03460c5c3f768d95a2",
+    "url": "/sonika/static/media/e15.ea629b05.jpg"
+  },
+  {
+    "revision": "c06856bc254ffff0d7add5aba17e3352",
+    "url": "/sonika/static/media/e16.c06856bc.jpg"
+  },
+  {
+    "revision": "0215d4400afa60e51815b19bce7ba55a",
+    "url": "/sonika/static/media/e17.0215d440.jpg"
+  },
+  {
+    "revision": "5c5a3b94c199d530e2c5b8f23090e390",
+    "url": "/sonika/static/media/e18.5c5a3b94.jpg"
+  },
+  {
+    "revision": "b0210f67da1eab9d3c1700687bb8a0e7",
+    "url": "/sonika/static/media/e19.b0210f67.jpg"
+  },
+  {
+    "revision": "98e730c6342f27e8133caba44cf53ab2",
+    "url": "/sonika/static/media/e2.98e730c6.jpg"
+  },
+  {
+    "revision": "763ddb00967c10382a574fafa0999a94",
+    "url": "/sonika/static/media/e20.763ddb00.jpg"
+  },
+  {
+    "revision": "b82786ddd63b439ca3636b12e8705dab",
+    "url": "/sonika/static/media/e21.b82786dd.jpg"
+  },
+  {
+    "revision": "0e0ddb27927ee13e7975d8ffaeaeb3f8",
+    "url": "/sonika/static/media/e22.0e0ddb27.jpg"
+  },
+  {
+    "revision": "97258d8be9f6a3d44ce077ec7179fa70",
+    "url": "/sonika/static/media/e23.97258d8b.jpg"
+  },
+  {
+    "revision": "0e71f435d735bb2fdae0418a8e03c79a",
+    "url": "/sonika/static/media/e24.0e71f435.jpg"
+  },
+  {
+    "revision": "c51890223350710ff3f19791f4471974",
+    "url": "/sonika/static/media/e25.c5189022.jpg"
+  },
+  {
+    "revision": "4ccd9898482a84e3b5b9fd5ae54866a7",
+    "url": "/sonika/static/media/e26.4ccd9898.jpg"
+  },
+  {
+    "revision": "3d62a01e617726ddba30aef4153e8efb",
+    "url": "/sonika/static/media/e27.3d62a01e.jpg"
+  },
+  {
+    "revision": "015d4e7389e0ebd23c0002274f653759",
+    "url": "/sonika/static/media/e28.015d4e73.jpg"
+  },
+  {
+    "revision": "77983265c6cbadf2a3bec4bc87ed380d",
+    "url": "/sonika/static/media/e29.77983265.jpg"
+  },
+  {
+    "revision": "9a2640a240ba89800d70c61c5c7c86f4",
+    "url": "/sonika/static/media/e3.9a2640a2.jpg"
+  },
+  {
+    "revision": "e01020720e0d32ff19b57038e8e6e992",
+    "url": "/sonika/static/media/e30.e0102072.jpg"
+  },
+  {
+    "revision": "76adee4a59567d0c59c64cd2870a4392",
+    "url": "/sonika/static/media/e31.76adee4a.jpg"
+  },
+  {
+    "revision": "1f0eafe6353c1d7a0d8fd4acdafd6dae",
+    "url": "/sonika/static/media/e32.1f0eafe6.jpg"
+  },
+  {
+    "revision": "c506fa0ce0c0b279ec90ca8b21d20222",
+    "url": "/sonika/static/media/e33.c506fa0c.jpg"
+  },
+  {
+    "revision": "4d89b7fcadc875805d59995710817b3b",
+    "url": "/sonika/static/media/e4.4d89b7fc.jpg"
+  },
+  {
+    "revision": "5c303ff07b083690cb754709a3b6293a",
+    "url": "/sonika/static/media/e5.5c303ff0.jpg"
+  },
+  {
+    "revision": "f00e1f3a9257d592a205c0f86bd35d90",
+    "url": "/sonika/static/media/e6.f00e1f3a.jpg"
+  },
+  {
+    "revision": "22799663f4976e3bee8f7bca4a7a479f",
+    "url": "/sonika/static/media/e7.22799663.jpg"
+  },
+  {
+    "revision": "cb0c49a37a77cfb2703a2f5c9c70236a",
+    "url": "/sonika/static/media/e8.cb0c49a3.jpg"
+  },
+  {
+    "revision": "ecf87fdafcbc1345942c19503e630691",
+    "url": "/sonika/static/media/e9.ecf87fda.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/sonika/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sonika/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sonika/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sonika/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sonika/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sonika/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/sonika/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/sonika/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/sonika/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/sonika/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "bd80794fddbffb4031fab537cc898680",
+    "url": "/sonika/static/media/myImage.bd80794f.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ekta/precache-manifest.d27c94d58cbbb72dc603473959b30f6b.js"
+  "/sonika/precache-manifest.21f0137be780c8a1d55a87aff371708a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ekta/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sonika/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
